5e28fa3d546865a6147b3946d6668b786c0c5aa1
林祐也_Hayashi <osalusan.ok@gmail.com>
2025-11-22 16:32:55 +0900

    ビルド

--- Assets/Orii/Script/TitleSceneManager.cs
+++ Assets/Orii/Script/TitleSceneManager.cs
@@ -14,7 +14,10 @@ public class TitleSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadScene();
+        }
     }
 
 
